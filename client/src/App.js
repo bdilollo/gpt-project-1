c1f3450b4ba5c82ba6747260ea394ff3fff095fe
bdilollo <113383715+bdilollo@users.noreply.github.com>
2023-07-12 08:48:55 -0500

    side menu + weird git bug

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import './normal.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <aside className='sidemenu'>
+        <div className='side-menu-button'>
+          <span>+</span>
+          New Chat
+        </div>
+      </aside>
+      <section className='chatbox'>
+
+      </section>
+    </div>
+  );
+}
+
+export default App;
